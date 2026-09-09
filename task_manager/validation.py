ea4156b1f49d68c8a380cd1db4f8c666ce4ad4eb
imaan <imaanally123@gmail.com>
2026-09-09 15:40:53 +0300

    Fix validation checks

--- task_manager/validation.py
+++ task_manager/validation.py
@@ -4,12 +4,17 @@ def validate_task_title(title):
     if len(title) == 0:
         print("Title cannot be empty.")
         return False
+
     return True
 
 def validate_task_description(description):
     if len(description) == 0:
         print("Description cannot be empty.")
         return False
+
+    if len(description) > 500:
+        raise ValueError("Description cannot be more than 500 characters.")
+
     return True
 
 def validate_due_date(due_date):
